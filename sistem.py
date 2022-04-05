6adebd0c9a12216db4ece478c47c1092e9a1280a
Leonardo Forgiarini Guedes <43677896+leonardofguedes@users.noreply.github.com>
2022-04-05 10:33:42 -0300

    Update sistem.py

--- sistem.py
+++ sistem.py
@@ -1,37 +1,43 @@
 from appstudent import add_student, students_total
-from appclass import add_class, class_total
+from appteacher import add_teacher, teacher_total
+from analysis import analysis_student, analysis_classes,show_list
 
 def welcome():
-    call = str(input('What do you want to add? [Class][Student]')).strip().capitalize()
-    if call == 'Student' or call == 'Class':
-        if call == 'Student':
-            add_student()
-        if call == 'Class':
-            add_class()
-    else:
-        print('Wrong answer.')
-
-
-
-def show():
-    ask = str(input('Do you want to see a list of Students or Classes? [Y][N]')).upper().strip()
-    if ask == 'Y':
-        ask2 = str(input('Which one?')).strip().capitalize()
-        if ask2 == 'Students' or ask2 == 'Classes':
-            if ask2 == 'Students':
-                print(students_total)
-            if ask2 == 'Classes':
-                print(class_total)
+    while True:
+        call = str(input('What do you want to add? [Teacher][Student][End adding]')).strip().capitalize()
+        if call == 'Student' or call == 'Teacher':
+            if call == 'Student':
+                add_student()
+            if call == 'Teacher':
+                add_teacher()
         else:
-            print('Wrong answer.')
-    else:
-        pass
+            break
+
+
+def analy():
+    while True:
+        q = str(input('Do you want to analyze the added numbers? [Y][N]')).strip().upper()
+        if q == 'Y':
+            while True:
+                q1 = str(input('Classes[CL]| Students[ST]| Teachers[TE]')).upper().strip()
+                if q1 == 'CL':
+                    analysis_classes()
+                if q1 == 'ST':
+                    analysis_student()
+                if q1 == 'TE':
+                    pass
+                else:
+                    break
+        else:
+            break
+
 
 def tela():
     while True:
         welcome()
-        show()
-        con = str(input('Continue? -> [Y][N]')).strip().upper()
+        show_list()
+        analy()
+        con = str(input('Continue with adding? -> [Y][N]')).strip().upper()
         if con == 'N':
             break
 
