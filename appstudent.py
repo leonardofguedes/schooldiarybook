ba3503763eb2a4aae8b3c1b788f96722042699d3
Leonardo Forgiarini Guedes <43677896+leonardofguedes@users.noreply.github.com>
2022-04-05 10:31:51 -0300

    Update appstudent.py
    I start to create lists to analyse the data.

--- appstudent.py
+++ appstudent.py
@@ -1,15 +1,23 @@
 from main import Students
 
 students_total = []
+students_classes = []
+students_average = []
 
 def add_student():
 
 
     add = str(input('Student name: ->')).strip().capitalize()
-    add2 = int(input('Student age: ->'))
-    add3 = int(input('Student class: ->'))
+    if add == '':
+        add = 'Unknow'
+    add2 = input('Student age: ->')
+    if add2 == '':
+        add2 = 0
+    add3 = input('Student class: ->')
+    if add3 == '':
+        add3 = 'Unknow'
     student = Students(add,add2,add3)
     student = student.__str__()
     students_total.append(student)
-
-
+    students_classes.append(add3)
+    students_average.append(int(add2))
