894d6e67842f956af9450487762ad42e2892b215
Leonardo Forgiarini Guedes <43677896+leonardofguedes@users.noreply.github.com>
2022-04-06 19:04:15 -0300

    Update appstudent.py

--- appstudent.py
+++ appstudent.py
@@ -13,11 +13,12 @@ def add_student():
     add2 = input('Student age: ->')
     if add2 == '':
         add2 = 0
-    add3 = input('Student class: ->')
-    if add3 == '':
-        add3 = 'Unknow'
+    add3 = int(input('Student class: ->'))
     student = Students(add,add2,add3)
     student = student.__str__()
     students_total.append(student)
-    students_classes.append(add3)
+    if add3 in students_classes:
+        pass
+    else:
+        students_classes.append(add3)
     students_average.append(int(add2))
