026df1c3bad48b50fc313076b6b142ee10e89855
Leonardo Forgiarini Guedes <43677896+leonardofguedes@users.noreply.github.com>
2022-04-08 17:05:24 -0300

    Update appstudent.py

--- appstudent.py
+++ appstudent.py
@@ -6,19 +6,23 @@ students_average = []
 
 def add_student():
 
-
-    add = str(input('Student name: ->')).strip().capitalize()
-    if add == '':
-        add = 'Unknow'
-    add2 = input('Student age: ->')
-    if add2 == '':
-        add2 = 0
-    add3 = int(input('Student class: ->'))
-    student = Students(add,add2,add3)
-    student = student.__str__()
-    students_total.append(student)
-    if add3 in students_classes:
+    try:
+        add = str(input('Student name: ->')).strip().capitalize()
+        if add == '':
+            add = 'Unknow'
+        add2 = int(input('Student age: ->'))
+        if add2 == '':
+            add2 = 0
+        add3 = int(input('Student class: ->'))
+        student = Students(add,add2,add3)
+        student = student.__str__()
+        students_total.append(student)
+        if add3 in students_classes:
+            pass
+        else:
+            students_classes.append(add3)
+            students_average.append(int(add2))
+    except:
+        print('Due wrong info, the system will not add the student.')
         pass
-    else:
-        students_classes.append(add3)
-    students_average.append(int(add2))
+
