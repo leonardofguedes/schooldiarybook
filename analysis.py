f69aab2fe4d8da5cc6fbaaa810ad9a0e82aed6fb
Leonardo Forgiarini Guedes <43677896+leonardofguedes@users.noreply.github.com>
2022-04-08 17:04:40 -0300

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -4,20 +4,29 @@ import statistics
 
 def show_list():
     while True:
-        ask = str(input('Do you want to see a list of Students or Teachers? [Y][N]')).upper().strip()
-        if ask == 'Y':
-            ask2 = str(input('Which one?')).strip().capitalize()
-            if ask2 == 'Students' or ask2 == 'Teachers':
-                if ask2 == 'Students':
-                    print(f'The list of students\n{students_total}')
+        try:
+            ask = str(input('Do you want to see a list of Students or Teachers? [Y][N]')).upper().strip()
+            if ask == 'Y':
+                ask2 = str(input('Which one?')).strip().capitalize()
+                if ask2 == 'Students' or ask2 == 'Teachers':
+                    if ask2 == 'Students':
+                        if len(students_total) == 0:
+                            print('You do not have registered students.')
+                        else:
+                            print(f'The list of students\n{students_total}')
+                    if ask2 == 'Teachers':
+                        if len(teacher_total) == 0:
+                            print('You do not have registered teachers.')
+                        else:
+                            print(f'The list of teachers\n{teacher_total}')
+                else:
+                    print('Wrong option. Write Students or Teachers.')
+            elif ask == 'N':
                     break
-                if ask2 == 'Teachers':
-                    print(f'The list of teachers\n{teacher_total}')
-                    break
-            else:
-                print('Wrong answer.')
-        else:
-            break
+            elif ask != 'N' and ask != 'Y':
+                print('Wrong option. Choose Y or N.')
+        except:
+            print('Wrong option.')
 
 
 
@@ -27,18 +36,27 @@ def analysis_student():
         ask = int(input('Option [1][2][3]'))
         if ask == 1:
             return print(f'Your school have {len(students_total)} student(s).')
+            break
         if ask == 2:
-            return print(f'Your registered class(es) is(are) {students_classes}.')
-        if ask == 3:
-            print(f'The average age of registered student(s) is {round(statistics.mean(students_average),1)} years')
-            ask = str(input('Do you want to know how many students are above the average age? [Y][N]')).upper().strip()
-            if ask == 'Y':
-                res = filter(lambda x: x > statistics.mean(students_average), students_average)
-                return print(f'{len(list(res))} student(s)')
-            else:
+            if len(students_classes) == 0:
+                print('You do not have registered classes.')
                 break
-
-        else:
+            else:
+                return print(f'Your registered class(es) is(are) {students_classes}.')
+                break
+        if ask == 3:
+            if len(students_total) == 0:
+                return print('You do not have registered students.')
+            else:
+                print(f'The average age of registered student(s) is {round(statistics.mean(students_average),1)} years')
+                ask = str(input('Do you want to know how many students are above the average age? [Y][N]')).upper().strip()
+                if ask == 'Y':
+                    res = filter(lambda x: x > statistics.mean(students_average), students_average)
+                    return print(f'{len(list(res))} student(s)')
+                    break
+                else:
+                    break
+        elif ask > 3:
             print('You have to choose [1], [2] or [3].')
 
 
